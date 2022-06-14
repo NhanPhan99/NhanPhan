ed9fe0e29e0547b15876fdc080d817b92c25c2dc
NhanPhan99 <phantrongnhan911999@gmail.com>
2022-06-14 15:33:23 +0700

    first commit

---
+++ routes/login.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.render('login');
+});
+module.exports = router;
